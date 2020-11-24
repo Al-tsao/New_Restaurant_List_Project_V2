32f6070295e04d9e28c43af2b9e6d2ce4206ce49
AlTsao <al.tsao.yw@gmail.com>
2020-11-24 21:39:52 +0800

    mongoose ENV set up

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser') //讓express可以用POST
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -33,6 +35,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //   }
 //   return options.inverse(this)
 // })
+
+// mongoDB資料連線
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // =========== routes setting Start ===========
 // 進入index頁面
