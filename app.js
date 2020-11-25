64d0a8206dce351916de8e83666de1a392fdc305
AlTsao <al.tsao.yw@gmail.com>
2020-11-25 10:33:20 +0800

    searh function done

--- app.js
+++ app.js
@@ -70,13 +70,18 @@ app.get('/show/:id', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理)
 })
 //搜尋餐廳並將結果列表顯示 
-// app.get('/search', (req, res) => {
-//   const keyword = req.query.keyword
-//   const restaurants = restaurantList.results.filter(restaurant => {
-//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-//   })
-//   res.render('index', { restaurants: restaurants, keyword: keyword })
-// })
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  listGenerated.find()
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(rList /* rList是清理過後的陣列 */ => {
+      const restaurants = rList.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+      })
+      res.render('index', { restaurants: restaurants, keyword: keyword })
+    }) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
 // =========== routes setting End ===========
 
 // start and listen on the Express server
