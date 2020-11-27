35db516966511a8d50ca696fcbaaadad23da098d
AlTsao <al.tsao.yw@gmail.com>
2020-11-27 16:04:55 +0800

    update function done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const exphbs = require('express-handlebars')
 // const restaurantList = require('./restaurant.json')
 
 // 為了要使用register helper而引入
-// const handlebars = require('handlebars')
+const handlebars = require('handlebars')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -30,12 +30,12 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // register helper
-// handlebars.registerHelper('ifEqual', function (job, targetJob, options) {
-//   if (job === targetJob) {
-//     return options.fn(this)
-//   }
-//   return options.inverse(this)
-// })
+handlebars.registerHelper('ifEqual', function (job, targetJob, options) {
+  if (job === targetJob) {
+    return options.fn(this)
+  }
+  return options.inverse(this)
+})
 
 // mongoDB資料連線
 // 取得資料庫連線狀態
@@ -59,13 +59,13 @@ app.get('/', (req, res) => {
     }) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
-// 進入show頁面
-app.get('/show/:id', (req, res) => {
+// 進入read頁面
+app.get('/read/:id', (req, res) => {
   const id = req.params.id
   listGenerated.findById(id)
     .lean()
     .then(rList => {
-      res.render('show', { restaurant: rList })
+      res.render('read', { restaurant: rList })
     }) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理)
 })
@@ -82,12 +82,12 @@ app.get('/search', (req, res) => {
     }) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
-// 進入new頁面
-app.get('/new', (req, res) => {
-  res.render('new')
+// 進入create頁面
+app.get('/create', (req, res) => {
+  res.render('create')
 })
-// 送出new頁面資料到MongoDB(新增資料)
-app.post('/new/create', (req, res) => {
+// 送出create頁面資料到MongoDB(新增資料)
+app.post('/create/new', (req, res) => {
   const newRestaurenat = req.body       // 從 req.body 拿出表單裡的 name 資料
   return listGenerated.create(newRestaurenat)     // 存入資料庫，create(這裡面的格式要是物件)
     .then(() => res.redirect('/')) // 新增完成後導回首頁
@@ -100,6 +100,30 @@ app.get('/delete/:id', (req, res) => {
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+// 進入update頁面
+app.get('/update/:id', (req, res) => {
+  const id = req.params.id
+  return listGenerated.findById(id)
+    .lean()
+    .then(restaurant => {
+      res.render('update', { restaurant: restaurant })
+    })
+    .catch(error => console.log(error))
+})
+// 送出update頁面資料到MongoDB(修改資料)
+app.post('/update/confirm/:id', (req, res) => {
+  //取得restaurant_id
+  const id = req.params.id
+  const options = req.body
+  return listGenerated.findById(id)
+    .then((restaurant) => {
+      //對應資料，寫入資料庫
+      restaurant = Object.assign(restaurant, options)
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/update/${id}`))
+    .catch((error) => console.log(error))
 })
 // =========== routes setting End ===========
 
