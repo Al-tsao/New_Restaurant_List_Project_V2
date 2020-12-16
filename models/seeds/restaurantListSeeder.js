4c78e45e018c295a0d2e3eb4d67a58697bbb6fe6
AlTsao <al.tsao.yw@gmail.com>
2020-12-16 20:28:27 +0800

    revise seeder and modal function

--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -13,23 +13,22 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
-  restaurantList.results.forEach(element => {
-    listGenerated.create(element) //
-  })
+  // restaurantList.results.forEach(element => {
+  //   listGenerated.create(element) //
+  // })
 })
 
-db.close()
 
-// const list = []
+const list = []
 
-// restaurantList.results.forEach(element => {
-//   list.push(element)
-// })
+restaurantList.results.forEach(element => {
+  list.push(element)
+})
 
-// listGenerated.create(list
-// ).then(() => {
-//   db.close()
-//   console.log("restauranSeed done!")
-// })
+listGenerated.create(list
+).then(() => {
+  db.close()
+  console.log("restauranSeed done!")
+})
 
 // 反正Schema裡面的設定就是一個物件，所以直接把restaurant.json中的物件拋進去就可以了，而且JSON的項目可以比較多，Schema的項目可以比較少
